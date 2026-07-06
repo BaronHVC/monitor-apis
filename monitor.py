8db0f6f304e51f732f7e7d44c73139e973982521
Carlos Eduardo <HVCE_Valley@outlook.com>
2026-07-05 23:28:11 -0600

    Agrega API de prueba al monitoreo

--- monitor.py
+++ monitor.py
@@ -30,6 +30,7 @@ load_dotenv()
 URLS = [
     "https://api.github.com",              # API pública de GitHub
     "https://pokeapi.co/api/v2/pokemon/1", # API pública de Pokémon
+    "https://api-que-no-existe-123.com"
 ]
 
 INTERVALO_SEGUNDOS = 300   # cada cuánto revisar (s)
